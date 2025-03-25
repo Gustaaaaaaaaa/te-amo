ab9dd46a28accfe852202bcc284926511c47b147
Gustavo <gustavozamboni07@gmail.com>
2025-03-25 00:11:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+/* Estilo do modal */
+.modal {
+    display: none;
+    position: fixed;
+    z-index: 1;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.7); /* Fundo escuro */
+    overflow: auto;
+    cursor: pointer;
+}
+
+.modal-content {
+    display: block;
+    margin: auto;
+    max-width: 80%;
+    max-height: 80%;
+    object-fit: contain;
+    border-radius: 10px;
+}
